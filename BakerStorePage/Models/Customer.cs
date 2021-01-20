675186418d3598557d0020d2a490f84bbf74eefc
Tue Thomas Ha Minh Son <thom875f@edu.eal.dk>
2021-01-20 08:40:09 +0100

    Version 0.11.6

--- BakerStorePage/Models/Customer.cs
+++ BakerStorePage/Models/Customer.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BakerStorePage.Models
+﻿namespace BakerStorePage.Models
 {
     public class Customer
     {
-        public int Id;
-        public string FirstName;
-        public string LastName;
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Customer(int customerId, string firstName, string lastName)
+        {
+            CustomerId = customerId;
+            FirstName = FirstName;
+            LastName = LastName;
+        }
     }
 }
